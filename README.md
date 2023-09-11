86a0ed86fcfe71d0ae0df91755c30aab01c231b2
Hulenkius <hulenkius@qq.com>
2023-09-11 13:57:29 +0800

    上傳

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # zu-zi.js
 網頁擴展字便捷顯示腳本
+
+本腳本利用徧黑體 SVG 庫和 Glyphwiki，實現網頁擴展字的便捷顯示。
+
+[DEMO](https://vistudium.top/1926/08/17/!test/)
+
+# 使用
+
+在 html 中引用 zu-zi.js。
+
+將擴展字用高尖括號 ⟨⟩ 包起來，腳本會將其處理成徧黑體中的字形。徧黑體中未完成的字則會顯示爲國標宋體。返回的圖像帶有「class="plgo"」屬性。<br>
+使用如：⟨𩽾⟩ ⟨𩾌⟩
+
+將 IDS 表達式用高尖括號 ⟨⟩ 包起來，腳本會將其處理成 Glyphwiki 中含有的字形。如果 Glyphwiki 中沒有此字，則會顯示成豆腐。返回的圖像帶有「class="zi"」屬性。<br>
+使用如：⟨⿰石㐫⟩
+
+將 Glyphwiki 字形名用高尖括號內的方括號 ⟨[]⟩包起來，腳本會將其處理成 Glyphwiki 中含有的同名字形。返回的圖像帶有「class="zi"」屬性。<br>
+使用如：⟨[dkw-27743]⟩ ⟨[u24756@12]⟩ ⟨[hulenkius_chujian-ly]⟩
+
+爲了顯示美觀，可以在網站 css 中加入以下內容：
+
+```
+.zi{
+	height: 1.1em;
+	position: relative;
+	top: 0.15em;
+	margin: -0.05em;
+}
+.plgo {
+    height: 0.92em;
+    position: relative;
+    top: .12em;
+    margin: 0.042em;
+}
+```
---
+++ zu-zi.js
@@ -0,0 +1,91 @@
+(function () {
+	// 使用正则表达式匹配文本中的尖括号 ⟨⟩ 包围的内容
+	const expressionRegex = /⟨(.*?)⟩/g;
+      
+	// 找到页面中的所有元素节点
+	const elementNodes = document.createTreeWalker(
+	  document.body,
+	  NodeFilter.SHOW_ELEMENT,
+	  null,
+	  false
+	);
+      
+	// 遍历每个元素节点
+	while (elementNodes.nextNode()) {
+	  const element = elementNodes.currentNode;
+	  let modifiedText = element.innerHTML;
+      
+	  // 使用正则表达式匹配元素节点中的表达式
+	  const matches = element.innerHTML.matchAll(expressionRegex);
+      
+	  for (const match of matches) {
+	    const expression = match[1];
+      
+	    // 判断表达式中是否包含特定字符
+	    const hasSpecialCharacters = /[⿰⿱⿲⿳⿴⿵⿶⿷⿸⿹⿺⿻]/.test(expression);
+      
+	    // 构建图像地址和 class 名称
+	    let imageUrl = '';
+	    let className = '';
+	    if (hasSpecialCharacters) {
+	      const codePoints = expression
+		.split('')
+		.map((char) => 'u' + char.codePointAt(0).toString(16).toLowerCase())
+		.join('-');
+	      imageUrl = `https://glyphwiki.org/glyph/${codePoints}.svg`;
+	      className = 'zi';
+	    } else if (expression.includes('[')) {
+	      // 移除尖括号和方括号，直接使用内容作为链接
+	      const cleanedExpression = expression.replace(/[⟨⟩\[\]]/g, '');
+	      imageUrl = `https://glyphwiki.org/glyph/${cleanedExpression}.svg`;
+	      className = 'zi';
+	    } else {
+	      const codePoints = Array.from(expression)
+		.map((char) => char.codePointAt(0).toString(16).toLowerCase())
+		.join('-');
+	      imageUrl = `https://seeki.vistudium.top/SVG/${codePoints}.svg`;
+	      className = 'plgo';
+	    }
+      
+	    // 创建一个新的 <img> 元素
+	    const img = document.createElement('img');
+	    img.src = imageUrl;
+	    img.className = className;
+      
+	    // 将原始表达式保存为自定义属性
+	    img.setAttribute('data-original-expression', expression);
+      
+	    // 将匹配的表达式替换为对应的图像
+	    modifiedText = modifiedText.replace(match[0], img.outerHTML);
+	  }
+      
+	  // 更新元素节点的内容
+	  if (modifiedText !== element.innerHTML) {
+	    element.innerHTML = modifiedText;
+	  }
+	}
+      
+	// 添加复制事件监听器
+	document.addEventListener('copy', function (event) {
+	  const selection = window.getSelection();
+	  if (selection.rangeCount > 0) {
+	    const range = selection.getRangeAt(0);
+	    const container = document.createElement('div');
+	    container.appendChild(range.cloneContents());
+      
+	    // 替换复制的内容，将 SVG 图像替换为原始文本
+	    const images = container.querySelectorAll('.zi, .plgo');
+	    for (const image of images) {
+	      const originalExpression = image.getAttribute('data-original-expression');
+	      const textNode = document.createTextNode(`⟨${originalExpression}⟩`);
+	      image.replaceWith(textNode);
+	    }
+      
+	    event.clipboardData.setData('text/plain', container.textContent);
+      
+	    // 阻止默认复制操作
+	    event.preventDefault();
+	  }
+	});
+      })();
+      
